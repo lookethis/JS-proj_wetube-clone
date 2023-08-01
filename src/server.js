c32eec1e1fdd1ba1aefc0b253fb0d8a9ad6b667a
lookethis <chzhclzlszkfp@gmail.com>
2023-08-02 00:05:07 +0900

    [feat]express server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 import express from "express";
 
+const PORT = 4000;
 const app = express();
 
-const handleListening = () => console.log("Server listening on port 4000 😀");
+const handleHome = (req, res) => {
+  return res.send("<h1>Home</h1>");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
-app.listen(4000, handleListening);
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT} 😀`);
+
+app.listen(PORT, handleListening);
