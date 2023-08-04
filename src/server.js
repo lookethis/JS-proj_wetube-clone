f5c4818770883821c5101eaff633d94a7db1834b
lookethis <chzhclzlszkfp@gmail.com>
2023-08-04 23:59:16 +0900

    [feat]routers

--- src/server.js
+++ src/server.js
@@ -1,43 +1,16 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/userRouter";
 
 const PORT = 4002;
 const app = express();
+app.use(morgan("dev")); // middleware 중 하나인 morgan
 
-const methodlogger = (req, res, next) => {
-  console.log("METHOD: ", req.method, req.url);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
-const routeLogger = (req, res, next) => {
-  console.log("Path: ", req.path);
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("<h1>Home</h1>");
-};
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
-};
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
-
-app.use(methodlogger); // Middleware1
-app.use(privateMiddleware); // Middleware2
-
-app.get("/", routeLogger, handleHome);
-app.get("/login", routeLogger, handleLogin);
-app.get("/protected", routeLogger, handleProtected);
+app.use("/", globalRouter); // "/"경로에 접근시, globalRouter의 controller 찾게 함
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 😀`);
