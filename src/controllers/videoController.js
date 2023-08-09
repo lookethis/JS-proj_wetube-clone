af3c9de71884745726acf48c2743eff4a1d00e60
lookethis <chzhclzlszkfp@gmail.com>
2023-08-09 23:39:30 +0900

    [feat]simple video info upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -49,3 +49,23 @@ export const postEdit = (req, res) => {
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
+
+// video추가하는 controller
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+// video를 video array에 추가
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/"); // 위의 trending을 호출하게 됨
+};
