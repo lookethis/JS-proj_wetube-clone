bccfabf21ec93733fb4e379c67de68b209c72700
lookethis <chzhclzlszkfp@gmail.com>
2023-08-22 23:14:35 +0900

    [feat]add loggedIn

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,11 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "Join";
+
   if (password !== password2) {
     return res.status(400).render("join", {
       pageTitle,
@@ -17,19 +19,53 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    password2,
-    location,
-  });
-  return res.redirect("/login");
+
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      password2,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "",
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  // res.render("login", { pageTitle: "Login" });
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An accouunt with this username does not exists.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+  // session에 정보 추가
+  req.session.loggedIn = true;
+  req.session.user = user;
+  return res.redirect("/");
 };
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.render("Login");
 export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("see User");
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 app.use(morgan("dev")); // middleware 중 하나인 morgan
@@ -11,6 +13,15 @@ app.set("view engine", "pug"); // view engine을 pug로 set
 app.set("views", process.cwd() + "/src/views"); // 경로 수정
 app.use(express.urlencoded({ extended: true })); // express application이 form의 value들 이해할 수 있게 js로 변경
 
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
