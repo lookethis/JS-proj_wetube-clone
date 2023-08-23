56c7ff5099e3fba06ccd42c87cc132fa0f3c80f7
lookethis <chzhclzlszkfp@gmail.com>
2023-08-23 17:43:26 +0900

    [feat]create .env

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,5 @@
 export const localsMiddleware = (req, res, next) => {
-  res.session.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user;
   console.log(req.session);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -13,11 +14,17 @@ app.set("view engine", "pug"); // view engine을 pug로 set
 app.set("views", process.cwd() + "/src/views"); // 경로 수정
 app.use(express.urlencoded({ extended: true })); // express application이 form의 value들 이해할 수 있게 js로 변경
 
+// 브라우저가 백엔드와 상호작용할 때마다
+// 이 session이라는 middleware가 브라우저에 쿠키 전송
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: true,
     saveUninitialized: true,
+    cookie: {
+      maxAge: 20000,
+    },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
